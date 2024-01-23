2394324d1ad61f6ac702df1eed4e8a8025212dff
zikrisuanda11 <zikrisuanda998@gmail.com>
2024-01-23 08:05:59 +0800

    fix: image candidate

--- resources/js/Pages/vote/components/cardCandidate.jsx
+++ resources/js/Pages/vote/components/cardCandidate.jsx
@@ -4,8 +4,8 @@ import DrawerVote from "./drawer";
 
 export default function CardCandidate({ candidate_image, auth, candidate}) {
   return (
-    <div className="flex flex-col items-center gap-2 bg-white rounded-xl px-5 py-5 shadow-md">
-      <img src={candidate_image} alt="" className="h-80 rounded-xl" draggable="false" />
+    <div className="flex flex-col items-center gap-2 bg-white rounded-xl p-5 shadow-md">
+      <img src={candidate_image} alt="" className="h-fit lg:h-80 rounded-xl" draggable="false" />
       <div className="space-x-3">
         {auth && (
           <ModalVote candidate={candidate} />
