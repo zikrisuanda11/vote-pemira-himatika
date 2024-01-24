c90f9df894c1bd07c5abe9276ad8ac23ec16622d
zikrisuanda11 <zikrisuanda998@gmail.com>
2024-01-24 19:52:56 +0800

    update: vote button

--- resources/js/Pages/vote/components/cardCandidate.jsx
+++ resources/js/Pages/vote/components/cardCandidate.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ModalVote from "./modal";
 import DrawerVote from "./drawer";
 
-export default function CardCandidate({ candidate_image, auth, candidate}) {
+export default function CardCandidate({ candidate_image, candidate }) {
   return (
     <div className="flex flex-col items-center gap-2 bg-white rounded-xl p-5 shadow-md">
       <img src={candidate_image} alt="" className="h-fit lg:h-80 rounded-xl" draggable="false" />
       <div className="space-x-3">
-        {auth && (
-          <ModalVote candidate={candidate} />
-        )}
+        <ModalVote candidate={candidate} />
         <DrawerVote candidate={candidate} />
       </div>
     </div>
